e22302e62b76bca8cabc1c6f800c66ec4dfe3176
YantaiGao <gaoyantai_jsj@163.com>
2016-06-22 23:54:34 +0800

    2016-06-22

---
+++ No19_varAndMethod.py
@@ -0,0 +1,13 @@
+def vara_and_varb(var1,var2):
+	print "Your var1 = %d" %var1
+	print "Your var2 = %d" %var2
+	print "OK!"
+
+vara_and_varb(20,30)
+
+a = 20
+b = 50
+vara_and_varb(a,b)
+vara_and_varb(20,b + 10)
+vara_and_varb(a + 20,b + 10)
+vara_and_varb(10 + 5,6 + 10)
