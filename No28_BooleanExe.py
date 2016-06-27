c85dd6378efdf28924bf31180145c621334fa57e
YantaiGao <gaoyantai_jsj@163.com>
2016-06-27 15:43:22 +0800

    2016-06-27

---
+++ No28_BooleanExe.py
@@ -0,0 +1,22 @@
+#-*- coding:utf-8 -*-
+
+# True or False 需要大写
+b1 =  True and True
+print "True and True : " , b1
+
+print "False and True : " , False and True
+# 与使用英文单词 and
+print "1==1 and 2==1 : " , 1==1 and 2==1
+
+print " \"test\" == \"test\" : " ,"test" == "test"
+print "True and 1 == 1 : ",True and 1 == 1
+print "False and 0 != 0 : ",False and 0 != 0
+#或使用英文单词 or
+print "1==1 or 2!=1 : " ,1==1 or 2!=1
+print "True or 1==1 :" , True or 1==1
+print " \"test\" == \"testing\" : ","test" == "testing"
+print "\"test\" == 1","test" == 1 
+#非使用英文单词 not
+print "not (True and False) : ",not (True and False)
+print not (1==1 and 0 != 1)
+print not (1 != 10 and 3 == 4)
