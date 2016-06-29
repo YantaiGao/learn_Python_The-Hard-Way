c69621229b366d2108c3436e49fd92b9721392e1
YantaiGao <gaoyantai_jsj@163.com>
2016-06-29 23:52:29 +0800

    2016-06-28

---
+++ No34_visitList.py
@@ -0,0 +1,11 @@
+#-*- coding:utf-8-*-
+arr = ['bear','python','gyt','apple','alibba','baidu','HuaWei']
+
+#注意：下标从0开始
+print "arr[0] : ",arr[0]
+print "arr[1] : ",arr[1]
+print "arr[2] : ",arr[2]
+print "arr[5] : ",arr[5]
+
+for ele in arr:
+	print ele ,
