36d2f7fc86af6af540ce50c8ea14de61ebfbe9d7
YantaiGao <gaoyantai_jsj@163.com>
2016-06-29 23:39:03 +0800

    2016-06-28

--- No33_while.py
+++ No33_while.py
@@ -14,7 +14,8 @@ while i < 6:
 print "print the numbers:"
 
 for num in numbers:
-	print num
+	print num,
+print "\n"
 	
 #改进一，将while改成函数，将6变成变量
 
@@ -33,3 +34,27 @@ print "print the res numbers:"
 
 for nums in resArray:
 	print nums,
+print "\n"
+
+#改进二，将while改成函数，将6变成变量，将增长长度也变成变量
+
+def arrIncrease(stopNum,len):
+	i = 0
+	res = []
+	while i < stopNum:
+		res.append(i)
+		i = i+len
+	return res
+
+print "请输入终止数字：".decode('utf-8').encode('gbk')
+finIn = int(raw_input(">"))	
+print "请输入增长长度：".decode('utf-8').encode('gbk')
+lenIn = int(raw_input(">"))	
+
+resArray1 = arrIncrease(finIn,lenIn)
+print "print the res1 numbers:"
+
+for nums in resArray1:
+	print nums,
+	
+	
