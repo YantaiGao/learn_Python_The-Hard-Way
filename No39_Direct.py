4258d5db016fd845427b4e3486b00b66c23288da
YantaiGao <gaoyantai_jsj@163.com>
2016-07-01 22:50:01 +0800

    2016-07-01

--- No39_Direct.py
+++ No39_Direct.py
@@ -23,17 +23,56 @@ del stuff[2]
 print stuff
 print '*' * 40
 
-states = [
+#注意：使用中括号是不对的
+states = {
 	'Oregon':'OR',
 	'Florida':'FL',
 	'New York':'NY',
 	'California':'CA',
 	'Michigan':'MI'
-	
-]
+}
 
-cities = [
+cities = {
 	'CA':'San Francisco',
 	'MI':'Detroit',
 	'FL':'Jacksonville',
-]
+}
+# add some city
+cities['NY'] = 'New York'
+cities['OR'] = 'Portland'
+
+#print some cities
+print '*' * 40
+print "NY state has: ",cities['NY']
+print "OR state has: ",cities['OR']
+
+#print some state
+print '*' * 40
+print "Michigan upcase: ",states['Michigan']
+print "Florida upcase: ",states['Florida']
+
+#关联查询
+print '*' * 40
+print "Michigan has : ",cities[states['Michigan']]
+print "Florida has : ",cities[states['Florida']]
+
+#遍历 k v
+print '*' * 40
+for astr,up in states.items():
+	print "%s Up is %s ." %(astr,up)
+
+print '*' * 40
+for sta,city in cities.items():
+	print "State %s has %s ." %(sta,city)
+
+#安全获取	
+print '*' * 40
+state = states.get('Texas',None)
+
+if not state:
+	print "Sorry ,no Texas. "
+
+#获取默认值
+city = cities.get('TX','Does Not Exist')
+print "The　city for the state \'TX\' is : %s" %city
+	
